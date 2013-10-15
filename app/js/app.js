354ab3f71c7c829b671b174964eeae5a3a7b8a53
Peter Kinmond <peterkinmond@gmail.com>
2013-10-15 08:33:03 -0700

    Format app.js to prepare for moving controllers to separate file

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,18 @@
-angular.module('project', ['ngRoute', 'firebase']).
-  value('fbUrl', 'https://dirty-santa.firebaseio.com/').
-  factory('Presents', function(angularFireCollection, fbUrl) {
-    return angularFireCollection(new Firebase(fbUrl + 'presents'));
-  }).
-  config(function($routeProvider) {
-    $routeProvider.
-      when('/', { controller: InitialController, templateUrl: 'partials/initial.html' }).
-      when('/game', { controller: GameController, templateUrl: 'partials/game.html' }).
-      when('/stats', { controller: StatsController, templateUrl: 'partials/stats.html' }).
-      otherwise({ redirectTo: '/' });
-  });
+angular.module('project', [
+  'ngRoute',
+  'firebase'
+]).
+value('fbUrl', 'https://dirty-santa.firebaseio.com/').
+factory('Presents', function(angularFireCollection, fbUrl) {
+  return angularFireCollection(new Firebase(fbUrl + 'presents'));
+}).
+config(function($routeProvider) {
+  $routeProvider.
+    when('/', { controller: InitialController, templateUrl: 'partials/initial.html' }).
+    when('/game', { controller: GameController, templateUrl: 'partials/game.html' }).
+    when('/stats', { controller: StatsController, templateUrl: 'partials/stats.html' }).
+    otherwise({ redirectTo: '/' });
+});
 
 function InitialController($scope, $location, $timeout, angularFire, fbUrl) {
   angularFire(new Firebase(fbUrl + 'game'), $scope, 'game', {}).
